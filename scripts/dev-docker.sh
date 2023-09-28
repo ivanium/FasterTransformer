b5e50f981f8ffd9e35fcf5fcb67822d21c650f8e
Yifan Qiao <yifanqiao@ucla.edu>
2023-09-28 12:37:15 -0700

    chore [scripts]: customized MODELS_DIR in dev-docker.sh

--- scripts/dev-docker.sh
+++ scripts/dev-docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR=$( cd -- "$SCRIPT_DIR/.." &> /dev/null && pwd )
@@ -7,6 +6,8 @@ ROOT_DIR=$( cd -- "$SCRIPT_DIR/.." &> /dev/null && pwd )
 IMAGE_NAME=manifold_base
 # IMAGE_NAME=nvcr.io/nvidia/pytorch:22.09-py3
 CONTAINER_NAME=manifold-dev-$USER
+
+MODELS_DIR=/scratch/manifold/FasterTransformer/models
 
 start_docker() {
     docker_running=$(docker ps --format '{{.Names}}' | grep ${CONTAINER_NAME})
@@ -17,6 +18,9 @@ start_docker() {
             -v ${HOME}/.cache/huggingface:/root/.cache/huggingface \
             -v ${ROOT_DIR}/../manifold:/manifold \
             -v ${ROOT_DIR}:/FasterTransformer \
+            -v ${ROOT_DIR}:${ROOT_DIR} \
+            -v ${MODELS_DIR}:/FasterTransformer/models \
+            -v ${MODELS_DIR}:${ROOT_DIR}/models \
             ${IMAGE_NAME} /bin/bash
     fi
 }
