19a0dc09035fbd85404072c3f6b7fffe87203657
Shu Anzai <wisteria1729@gmail.com>
2023-10-12 14:28:32 -0700

    wip

--- scripts/eval_llama.sh
+++ scripts/eval_llama.sh
@@ -17,7 +17,10 @@ do
         output_file="${ROOT_DIR}/tmp/manifold_llama-"$batch"-"$output".log"
         sed -r 's#^request_batch_size=[0-9]+#request_batch_size='"$batch"'#; s#^request_output_len=[0-9]+#request_output_len='"$output"'#' ${ROOT_DIR}/models/llama2/1-gpu/config.ini > $config_file
         ${ROOT_DIR}/scripts/run_llama.sh "$config_file"  > "$output_file"
+        echo "done $batch $output"
     done
 done
+
+echo "done all the executions, now plotting"
 
 python3 plot_llama.py batch_initial batch_max output_initial output_max ${ROOT_DIR}/tmp
